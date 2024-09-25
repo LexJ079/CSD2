8c4ca98cf85a2a9ca75e0a93ca55e9f8986e0520
Lex Jellema <jellemalex079@gmail.com>
2024-09-25 14:07:10 +0200

    First version of the rythmic playback exercise done.

--- CSD2a/rythmicplayback/src/rythmicplayback.py
+++ CSD2a/rythmicplayback/src/rythmicplayback.py
@@ -1,15 +1,34 @@
 import simpleaudio as sa
-
-# Vraagt hoe vaak het geluid moet worden afgespeeld
-numPlaybackTimes = int(input("Hoe vaak moet het geluid afspelen? "))
-
-# Laadt de audio file (synth.wav)
-wave_obj = sa.WaveObject.from_wave_file("synth.wav")
-
-# Speelt het geluid het opgegeven aantal keren af
-for i in range(numPlaybackTimes):
-    play_obj = wave_obj.play()  # Speelt de audio af
-    play_obj.wait_done()  # Wacht tot de audio is afgelopen
-   
+import time
 
 
+
+# vragen naar hoeveelheden en tijden
+numRepeats = int(input("Hoevaak wil je het ritme horen?: "))
+
+numNotes = int(input("Hoeveel noten in het ritme? "))
+
+noteDurs = []
+for i in range(numNotes):
+    noteDur = float(input(f"Welke tijdwaarde heeft noot nummer {i + 1}? "))
+    noteDurs.append(noteDur)
+
+bpm = float(input("Please enter the bpm: "))
+
+
+
+# verandert de nootwaarden in tijdwaarden
+quarternoteDur = 60 / bpm
+timeDurs = []
+for noteDur in noteDurs:
+    timeDurs.append(noteDur * quarternoteDur)
+
+
+
+# sample laden en herhalen
+sample = sa.WaveObject.from_wave_file("sample.wav")
+
+for _ in range(numRepeats):
+    for tDur in timeDurs:
+        sample.play().wait_done()
+        time.sleep(tDur)
